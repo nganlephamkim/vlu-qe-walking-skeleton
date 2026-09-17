19cb2539a8c8ab406478803289a62fd08fb0af5e
nganlephamkim <ngan.lephamkim@gmail.com>
2026-09-17 14:06:20 +0700

    login.js

--- login.js
+++ login.js
@@ -1,8 +1,19 @@
+```javascript
 function login(username, password) {
-  return username === "admin" && password === "123";
+    return username === "admin" && password === "123";
+}
+
+function checkLogin() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const result = login(username, password);
+
+    document.getElementById("result").innerText = result;
 }
 
 // Dùng cho Jest trong môi trường Node.js
 if (typeof module !== "undefined" && module.exports) {
-  module.exports = login;
+    module.exports = login;
 }
+```
